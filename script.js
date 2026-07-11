1a1f5e9800b52a74af0dcf1336f14d7e5ee6367f
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-07-11 14:53:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-// Person Constructor
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
-// greet method
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
-// Employee Constructor
 function Employee(name, age, jobTitle) {
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Inherit from Person
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// jobGreet method
 Employee.prototype.jobGreet = function () {
   console.log(
     `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
